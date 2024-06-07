60922d964ce532a3c8a9fed716754e95e23ddd83
ra1ph <kneshkreba@mail.ru>
2024-06-07 23:02:36 +0300

    feature/get-news-all-and-by-id

--- backend/internal/app/endpoint/news/news.go
+++ backend/internal/app/endpoint/news/news.go
@@ -1,36 +1,57 @@
 package news
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"google.golang.org/grpc"
 	pb "moscowhack/gen/go/news"
 	"strconv"
 )
 
-type Service interface {
-	GetNewsFromDB(id int) (*pb.NewsItem, error)
+type News interface {
+	GetNews() (*pb.NewsItem, error)
+	GetNewsById(id int) (*pb.NewsItem, error)
 }
 
 type Endpoint struct {
-	s      Service
-	server NewsServiceServer
-}
-
-type NewsServiceServer struct {
+	getNews News
 	pb.UnimplementedNewsServiceServer
 }
 
-func New(s Service) *Endpoint {
+func Register(gRPCServer *grpc.Server, news News) {
+	pb.RegisterNewsServiceServer(gRPCServer, &Endpoint{getNews: news})
+}
+
+func New(news News) *Endpoint {
 	return &Endpoint{
-		s: s,
+		getNews: news,
 	}
 }
 
-func (e *Endpoint) NewsById(req *pb.NewsRequest) (*pb.NewsResponse, error) {
+func (e *Endpoint) GetNews(ctx context.Context, req *pb.NewsRequest) (*pb.NewsResponse, error) {
+	fmt.Println(e, " ААА ", e.getNews)
+	newsData, err := e.getNews.GetNews()
+	if err != nil {
+		return &pb.NewsResponse{}, err
+	}
+
+	newsItem := map[string]*pb.NewsItem{"NewsItem": newsData}
+
+	return &pb.NewsResponse{News: newsItem}, nil
+}
+
+func (e *Endpoint) GetNewsById(ctx context.Context, req *pb.NewsRequest) (*pb.NewsResponse, error) {
+	if req.Id == "" {
+		return nil, errors.New("id новости не указан")
+	}
+
 	id, err := strconv.Atoi(req.Id)
 	if err != nil {
 		return &pb.NewsResponse{}, err
 	}
 
-	newsData, err := e.s.GetNewsFromDB(id)
+	newsData, err := e.getNews.GetNewsById(id)
 	if err != nil {
 		return &pb.NewsResponse{}, err
 	}
