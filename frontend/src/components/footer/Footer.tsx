c66c32b43ae7f7c9395e700efbe88a19a887833c
yourassarered <eonezie2006@gmail.com>
2024-06-09 00:24:39 +0300

    add/ footer

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -4,26 +4,26 @@ import classes from "./Footer.module.scss";
 const Footer = () => {
     return (
         <footer className={classes.footer}>
-            <div className={classes.column}>
+            <div className={classes.columnLeft}>
                 <h3>Про Reksoft</h3>
                 <ul>
                     <li><a href="#">Главная страница</a></li>
                     <li><a href="#">Контакты</a></li>
                 </ul>
             </div>
-            <div className={classes.column}>
-                <h3>О разработке</h3>
+            <div className={classes.columnRight}>
+                <h3>Связатся с нами</h3>
                 <ul>
                     <li><a href="#">97jertov97@gmail.com</a></li>
                     <li><a href="#">1lyaaksenov@mail.com</a></li>
                 </ul>
             </div>
-            <div className={classes.column}>
+            <div className={classes.columnRight}>
                 <h3>Колонка 3</h3>
                 <ul>
-                    <li><a href="#">Ссылка 1</a></li>
-                    <li><a href="#">Ссылка 2</a></li>
-                    <li><a href="#">Ссылка 3</a></li>
+                    <li>
+                       <a>Сделанно командой малосольные огурчики в рамках Хакатона: "Лидеры цифровой трансформации"</a>
+                    </li>
                 </ul>
             </div>
         </footer>
