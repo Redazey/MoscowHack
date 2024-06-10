c80bd9832cce2f7e2a4c430c3bbfe7f052fc8ae7
yourassarered <eonezie2006@gmail.com>
2024-06-10 19:05:05 +0300

    fix/Fronttend-structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react'
 import Header from "./components/header/Header.tsx";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Footer from "./components/footer/Footer.tsx";
 
 
 function App() {
 
   return (
-      <>
-          <BrowserRouter>
-              <div>
-                  <main>
-                      <Routes>
-                          <Route path="/" element={<Header />} />
-                          {/* <Route path="/id/:id" element={<Movie />} /> */}
-                      </Routes>
-                  </main>
-              </div>
-          </BrowserRouter>
-      </>
+      <Router>
+          <Header/>
+          <Footer/>
+      </Router>
   )
 }
 
