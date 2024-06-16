9406d1c7fcf0183a6713e34b4b6222ef0abb195b
Apep <mane130320006@gmail.com>
2024-06-16 18:45:22 +0300

    fix/registration

--- frontend/src/pages/registration_page.tsx
+++ frontend/src/pages/registration_page.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import './registration_style.scss';
 import Header from '../components/header/Header';
-import Footer_small from '../components/footer_small/footer_small';
 import { Link } from 'react-router-dom';
+import Footer_small from '../components/footer_small/footer_small';
 
 const RegistrationForm: React.FC = () => {
     const [form, setForm] = useState({
@@ -24,46 +24,53 @@ const RegistrationForm: React.FC = () => {
 
     return (
         <>
-            <Header/>
-            <div className="registration-form">
-                <h2>Регистрация</h2>
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        name="firstName"
-                        placeholder="Имя"
-                        value={form.firstName}
-                        onChange={handleChange} />
-                    <input
-                        type="text"
-                        name="lastName"
-                        placeholder="Фамилия"
-                        value={form.lastName}
-                        onChange={handleChange} />
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="E-mail"
-                        value={form.email}
-                        onChange={handleChange} />
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Пароль"
-                        value={form.password}
-                        onChange={handleChange} />
-                    <input
-                        type="password"
-                        name="confirmPassword"
-                        placeholder="Повтор пароля"
-                        value={form.confirmPassword}
-                        onChange={handleChange} />
-                    <button type="submit">Создать аккаунт</button>
-                </form>
-                <div className="login-link">
-                    <span>Есть аккаунт?</span>
-                    <Link to="/Login">Авторизуйтесь</Link>
-                </div>
+            <Header />
+            <div className="auth-container">
+                <main className="main">
+                    <div className="auth-content">
+                        <div className="auth-box">
+                            <h2>Регистрация</h2>
+                            <form onSubmit={handleSubmit}>
+                                <input
+                                    type="text"
+                                    name="firstName"
+                                    placeholder="Имя"
+                                    value={form.firstName}
+                                    onChange={handleChange} />
+                                <input
+                                    type="text"
+                                    name="lastName"
+                                    placeholder="Фамилия"
+                                    value={form.lastName}
+                                    onChange={handleChange} />
+                                <input
+                                    type="email"
+                                    name="email"
+                                    placeholder="E-mail"
+                                    value={form.email}
+                                    onChange={handleChange} />
+                                <input
+                                    type="password"
+                                    name="password"
+                                    placeholder="Пароль"
+                                    value={form.password}
+                                    onChange={handleChange} />
+                                <input
+                                    type="password"
+                                    name="confirmPassword"
+                                    placeholder="Повтор пароля"
+                                    value={form.confirmPassword}
+                                    onChange={handleChange} />
+                                <button type="submit">Создать аккаунт</button>
+                            </form>
+                        </div>
+                        <div className="auth-box register-box">
+                            <p>
+                                Есть аккаунт? <Link to="/Login">Авторизуйтесь</Link>
+                            </p>
+                        </div>
+                    </div>
+                </main>
             </div>
             <Footer_small/>
         </>
